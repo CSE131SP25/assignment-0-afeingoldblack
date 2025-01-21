873b37cd2519397b0b97fac70477c1ce028f7aa9
Alex <Alex@localhost>
2025-01-21 12:51:09 -0600

    made a flower

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,23 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
+		robot.forward(150);
+		for (int i = 0; i < 20;) {
+		robot.turnLeft(150);
 		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.turnRight(50);
+		robot.forward(40);
+		robot.turnRight(50);
+		robot.forward(40);
+		robot.turnRight(50);
+		robot.forward(40);
+		robot.turnRight(50);
+		robot.forward(40);
+		robot.turnRight(50);
+		robot.forward(60);
+		i++;
+		
+	}
 	}
 
 }
